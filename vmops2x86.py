7f75837258729d5016350c8e45389b1837533601
msh1307 <86157260+msh1307@users.noreply.github.com>
2023-03-19 18:39:36 +0900

    -.-

---
+++ vmops2x86.py
@@ -0,0 +1,49 @@
+from capstone import *
+from keystone import *
+from tqdm import tqdm
+import itertools
+import struct
+vm_opcodes_args = [[0x81,1],[0x91,2],[0x9F,0],[0xA1,2],[0xAA,1],[0x3,1],[0x6,1],[0xD,2],[0x24,2],[0xd8,2],[0x73,2]]
+cs = Cs(CS_ARCH_X86, CS_MODE_64)
+OPS = []
+for oa in vm_opcodes_args:
+    a = [i for i in range(0x100)]
+    a *= oa[1]
+    op = oa[0].to_bytes(1,byteorder='little')
+    for tp in tqdm(itertools.permutations(a,oa[1])):
+        code = op + bytes(tp)
+        for k in cs.disasm(code,0x0):
+            flag = 1
+            for i in OPS:
+                if i[0] == k.mnemonic:
+                    flag = 0
+            if flag:
+                OPS.append([k.mnemonic,oa[1],oa[0]])
+if __name__ == "__main__":
+    print("--- Candidates ---")
+    for i in OPS:
+        print(i[0])
+    while True:
+        print("\nopcode > ",end='')
+        opcode = input()
+        if opcode == 'exit':
+            exit()
+        flag = 0
+        for i,j in enumerate(OPS):
+            if j[0] == opcode:
+                flag=1
+                idx=i
+                break
+        if flag:
+            a = [i for i in range(0x100)]
+            a *= OPS[idx][1]
+            op = OPS[idx][2].to_bytes(1,byteorder='little')
+            for i in tqdm(itertools.permutations(a,OPS[idx][1])):
+                code = op + bytes(i)
+                for k in cs.disasm(code,0x0):
+                    if k.mnemonic == opcode:
+                        print("%s   %s"%(k.mnemonic,k.op_str),end='')
+                        arr = [x for x in code]
+                        print('  //  '+str(arr))
+        else:
+            print("Not found")
